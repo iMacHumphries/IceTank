d447baf92fa127df1a192bbd2c3ad27080d25506
Benjamin Humphries <Ben@Benjamins-MacBook-Air.local>
2014-04-07 22:43:08 -0400

    Initial Commit

---
+++ ScrappyTank/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScrappyTank
+//
+//  Created by Benjamin Humphries on 4/7/14.
+//  Copyright (c) 2014 Benjamin Humphries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
